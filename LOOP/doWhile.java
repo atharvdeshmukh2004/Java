59861551cba3e207876f57ffe4ef3a67db098017
atharvdeshmukh2004 <datharv56@gmail.com>
2025-05-19 07:52:49 +0530

    Create doWhile.java

---
+++ LOOP/doWhile.java
@@ -0,0 +1,9 @@
+public class Do_while {
+    public static void main(String[] args) {
+        int i = 5;
+        do{
+            System.out.println("Namaste Java "+ i);
+            i++;
+        } while (i < 5);
+    }
+}
